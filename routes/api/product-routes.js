fd269b1b718c4f298a697315a1caa11f0ca43758
JDempe <123279032+JDempe@users.noreply.github.com>
2023-05-08 18:00:34 -0400

    Fixed product-routes to have Category/Tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -33,7 +33,7 @@ router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk(req.params.id, {});
+    const productData = await Product.findByPk(req.params.id, { include: [Category, Tag]});
 
     if (!productData) {
       res.status(404).json({ message: "Invalid product id." });

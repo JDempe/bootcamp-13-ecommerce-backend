4c3ee43c149c49864982806d47fd776920aa8811
JDempe <123279032+JDempe@users.noreply.github.com>
2023-05-08 14:07:09 -0400

    Fixed associations for database models

--- models/index.js
+++ models/index.js
@@ -6,30 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: {category_id: 'id'},
+  foreignKey: 'category_id',
   onDelete: 'CASCADE',
 });
 
-
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: {category_id: 'id'},
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: {product_id: 'id'},
-  onDelete: 'CASCADE',
+  foreignKey: 'product_id',
 });
 
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: {tag_id: 'id'},
-  onDelete: 'CASCADE',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
